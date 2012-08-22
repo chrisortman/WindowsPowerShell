3a1559b47a7273a4de5765393dfeeea24d4bd44b
Chris Ortman <chriso@innovsys.com>
2012-08-22 17:02:42 -0500

    Add some nuget confg + helpers

---
+++ NuGet_profile.ps1
@@ -0,0 +1,38 @@
+function Show-InUsePackages
+{
+    param($Filter = "")
+
+    if($Filter -eq "") 
+    {
+        $packages = (Get-Package)
+    }
+    else 
+    {
+        $packages = (Get-Package -Filter $Filter)
+    }
+
+    $real_output = "";
+
+    $packages | %{
+        $real_output += $_.Id + " " + $_.Version.ToString() +  "`n"
+        $real_output += "----------------------------------`n"
+
+        $currentPackage = $_;
+        get-project -All | %{
+            $packageInProject = (Get-Package -Filter $currentPackage.Id -Project $_.ProjectName)
+            if($packageInProject.Length -eq 1) {
+                $real_output += $packageInProject[0].Version.ToString() + "  " + $_.ProjectName.ToString() + "`n"
+            }
+        }
+    }
+
+    Write-Output $real_output
+
+}
+
+function Search-PackageConfigs
+{
+    param($searchText)
+
+    Get-ChildItem -Path . -Recurse -Include packages.config | Select-String $searchText
+}
