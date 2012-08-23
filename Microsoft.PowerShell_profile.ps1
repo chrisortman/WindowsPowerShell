294bc07088222e4922aeb52de175ee9a66f0aff6
Chris Ortman <chriso@innovsys.com>
2012-08-23 16:56:17 -0500

    Aliases for vi & gihub for windows

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,9 @@ $psroot = (Split-Path $profile)
 Set-Alias msbuild C:\windows\microsoft.net\framework\v4.0.30319\MSBuild.exe
 Set-Alias vs 'C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe'
 Set-Alias subl 'C:\Program Files\Sublime Text 2\sublime_text.exe'
+Set-Alias gh4w 'C:\Users\ChrisO\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\GitHub, Inc\GitHub.appref-ms'
+Set-Alias vi 'C:\Program Files (x86)\Vim\vim73\vim.exe'
+Set-Alias gvim 'C:\Program Files (x86)\Vim\vim73\gvim.exe'
 
 $mydir = (Split-Path -parent $MyInvocation.MyCommand.Definition)
 
