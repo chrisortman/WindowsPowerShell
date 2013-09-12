b321fdc641525e3f02f2f1e7d4ac8f00f184b19a
Chris Ortman <chriso@innovsys.com>
2013-09-12 10:31:21 -0500

    Module updates

---
+++ calculate-timespan.ps1
@@ -0,0 +1,11 @@
+﻿#$callTimes = ('00:01:09','00:00:07','00:07:22','01:21:16','00:07:03','00:00:06','00:00:11','00:00:06','01:21:28')
+$callTimes = ('00:01:19','00:00:55','00:06:02')
+$totalTicks = 0
+$callTimes | %{
+    $ts = [System.TimeSpan]::Parse($_);
+    $totalTicks += $ts.Ticks;
+}
+
+$tsTotal = new-object -TypeName System.TimeSpan -ArgumentList ($totalTicks / $callTimes.Length)
+
+$tsTotal
